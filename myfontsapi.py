11a1e412df48f48247365a86a4a468ef5b46f997
Marc Foley <marc.foley@daltonmaag.com>
2015-07-22 23:39:38 +0200

    add top search to api

--- myfontsapi.py
+++ myfontsapi.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import reqparse, Api, Resource
 from pymongo import MongoClient
 from bson.json_util import dumps
+from collections import Counter
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,12 +15,21 @@ parser.add_argument('task', type=str)
 
 
 class Top(Resource):
-        def get(self, table):
-                g = list(db[table].find())
+        def get(self, table, tag):
+            count = {}
+            counter = 0
+            for i in db[table].find():
+                if not i[tag] in count:
+                    count[i[tag]] = {}
+                    count[i[tag]]['total'] = 1
+                else:
+                    count[i[tag]]['total'] += 1
+                    count[i[tag]]['designer'] = i['designer']
+                    count[i[tag]]['name'] = i['name']
+                    count[i[tag]]['font_url'] = i['font_url']
+            return count
 
-                return dumps(g)
-
-api.add_resource(Top, '/<table>')
+api.add_resource(Top, '/<table>=<tag>')
 
 if __name__ == '__main__':
     app.run()
